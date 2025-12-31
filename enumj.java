ea6e0d786e8d8e89f634c868eceaf81e67bcaecd
Abhay Gondaliya <abhaygondaliya881@gmail.com>
2025-12-31 12:44:37 +0530

    Add files via upload
    enum

---
+++ enumj.java
@@ -0,0 +1,7 @@
+class enumj{
+	enum list{low,medium,high}
+	public static void main(String[] args){
+		list l=list.medium;
+		System.out.print(l);
+	}
+}
---
+++ strict.java
@@ -0,0 +1,7 @@
+class strict{
+	public static void main(String[] args){
+		double num1=10e+10;
+		double num2=6e+02;
+		System.out.println((num1+num2));
+	}
+}
