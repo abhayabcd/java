170d9db9f8516c487bf2d2b1fc972d0f7e1ee739
Abhay Gondaliya <abhaygondaliya881@gmail.com>
2026-01-01 12:15:58 +0530

    Add files via upload
    command line argument

---
+++ cmda.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class cmda{
+	public static void main(String[] args){
+		System.out.println(Arrays.toString(args));
+        	Arrays.sort(args);
+		System.out.println(Arrays.toString(args));
+	}
+}
