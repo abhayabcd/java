f78a3e358c65373ce618d7c195036b3f545cba7a
Abhay Gondaliya <abhaygondaliya881@gmail.com>
2026-02-05 08:23:10 +0530

    Add files via upload
    5/2/2026

---
+++ normal_import.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class normal_import{
+	
+	public static void main(String[] i){
+		System.out.println(Math.sqrt(4));
+		System.out.println(Math.pow(2,2));
+		System.out.println(Math.abs(-4));
+	}
+}
---
+++ static_import.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*;
+class static_import{
+	
+	public static void main(String[] i){
+		System.out.println(sqrt(4));
+		System.out.println(pow(2,2));
+		System.out.println(abs(-4));
+	}
+}
