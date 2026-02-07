abdc49e5004c44aba36545767f3b8a7ca84388b4
Abhay Gondaliya <abhaygondaliya881@gmail.com>
2026-02-07 08:12:04 +0530

    Add files via upload
    7/2/2026

---
+++ math.java
@@ -0,0 +1,18 @@
+import static java.lang.Math.*;
+public class math {
+
+    public static void main(String[] args) {
+        double a = 10.10;
+        double b = -5.0;
+        double c = 0.5; 
+        System.out.println("Absolute value of " + b + " is: " + abs(b));
+        System.out.println("Ceiling of " + c + " is: " + ceil(c));
+        System.out.println("Floor of " + c + " is: " + floor(c));   
+        System.out.println("Maximum of " + a + " and " + b + " is: " + max(a, b));
+        System.out.println("Minimum of " + a + " and " + b + " is: " + min(a, b));
+        System.out.println("Square root of " + a + " is: " + sqrt(a));
+        System.out.println("Power of " + a + " raised to 2 is: " + pow(a, 2));
+        
+        
+    }
+}
